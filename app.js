4fe1ba7a29536c8b2e6161e248e496f1c8463990
Faye Burke <fayeelizabethburke@gmail.com>
2019-09-02 11:39:33 +0000

    got the collapsable jumbotron working

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ let express = require("express"),
 	app = express();
 
 let array =[
-	        	{title: "title1", content: "content1", image: "image1"}, 
-                {title: "title2", content: "content2", image: "image2"}, 
-                {title: "title3", content: "content3", image: "image3"}, 
-                {title: "title4", content: "content4", image: "image4"}
+	        	{title: "title1", content: "content1", image: "https://www.gstatic.com/webp/gallery/3.jpg"}, 
+                {title: "title2", content: "content2", image: "https://www.gstatic.com/webp/gallery/4.jpg"}, 
+                {title: "title3", content: "content3", image: "https://images.unsplash.com/photo-1558403748-6d8a03390378?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}, 
+                {title: "title4", content: "content4", image: "https://pixabay.com/get/52e3d14a4b5ab108f5d084609620367d1c3ed9e04e50744f732e7bd4944fc4_340.jpg"}
 ];
 //express to extract the post request
 app.use(express.static(__dirname + '/public'));
